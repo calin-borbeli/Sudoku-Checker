7cae1dfceeef67c29e4d27f8370514eea3aa6cab
Calin <calin@calinborbeli.com.au>
2023-11-18 17:59:42 +1100

    Second version with tiles checker

--- sudoku.py
+++ sudoku.py
@@ -13,7 +13,6 @@ for i in range(9):
     # input the rows and append them to the list
     row = input("Enter row [" + str(i + 1) + "]: ")
     rows.append(row)
-print(rows)
 
 
 # Check that the rows contain only valid digits rows
@@ -22,9 +21,30 @@ for row in rows:
         print("Invalid row [" + str(rows.index(row)) + "]: '" + row + "'")
 
 # Check that the columns contain only valid digits columns
-for c in range(9):
-    column = "".join(rows[rc][c] for rc in range(9))
+for col in range(9):
+    column = "".join(rows[row_col][col] for row_col in range(9))
     if not digit_check(column):
-        print("Invalid column [" + str(c + 1) + "]: '" + column + "'")
+        print("Invalid column [" + str(col + 1) + "]: '" + column + "'")
 
 # Check that the 3x3 tiles contain only valid digits tiles
+for tile_rows in range(0, 9, 3):
+    for tile_cols in range(0, 9, 3):
+        tile = []
+        for rt in range(3):
+            for ct in range(3):
+                tile.append(rows[rt + tile_rows][ct + tile_cols])
+        tile = "".join(tile)
+        if not digit_check(tile):
+            print(
+                "Invalid tile ["
+                + str(tile_cols + 1)
+                + "]["
+                + str(tile_rows + 1)
+                + "] to ["
+                + str(ct + tile_cols + 1)
+                + "]["
+                + str(rt + tile_rows + 1)
+                + "]: '"
+                + tile
+                + "'"
+            )
